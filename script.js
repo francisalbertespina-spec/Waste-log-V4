94bfd30238bb75e6c09bea653526c717ecea9c35
francisalbertespina-spec <francisalbertespina@gmail.com>
2026-02-05 09:46:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ window.isUploading = false;
 
 const DEV_MODE = false; // Set to false for production
 
-const scriptURL = "https://script.google.com/macros/s/AKfycbyFr_neafwa6_dfrK-eWTv_xqN_MISJqBq8vSpTuKznk1SdplkVYBZY72wLb5KKAi-4/exec";
+const scriptURL = "https://script.google.com/macros/s/AKfycbxDEhVsVb4Zrol_32MUbR3YHXJ6Ai2JQFKphZ9UnG_oHULrjoC1JftOCYZUlTX24fRn/exec";
 
 function showToast(message, type = "info", options = {}) {
   const { persistent = false, spinner = false, duration = 3000 } = options;
