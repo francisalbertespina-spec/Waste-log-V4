617e03ee2d92b78997537c78275f556aadd1527f
francisalbertespina-spec <francisalbertespina@gmail.com>
2026-02-13 17:42:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -115,7 +115,8 @@ async function authenticatedFetch(url, options = {}) {
 
 const DEV_MODE = false; // Set to false for production
 
-const scriptURL = "https://script.google.com/macros/s/AKfycbyL27Vko3QfF9ENnRUxPAyN1y00Jv-W6VTuverYEVBleLm9pLCCn8V6r00MZK1wMUUe/exec";
+const scriptURL = "https://script.google.com/macros/s/AKfycbz34e5qq-anNzGEhy0ZAVEtubBufD3JRgmTsaYgbpTe40xd5Sc5uwAhoogzxObjeav4/exec";
+// const scriptURL = "https://script.google.com/macros/s/AKfycbyL27Vko3QfF9ENnRUxPAyN1y00Jv-W6VTuverYEVBleLm9pLCCn8V6r00MZK1wMUUe/exec";
 // const scriptURL = "https://script.google.com/macros/s/AKfycbwOzLtzZtvR2hrJuS6uVPe58GxATwtwwkSJ_yP073vST9B3283AYd7ADG8ApmPuDKJO/exec";
 // Stable V4 - const scriptURL = "https://script.google.com/macros/s/AKfycbxe2nDYZzBT8QCsp_XQa0RaV36c0MMUAYDdrwwGydSs0AbQ1H7RlbGHyE8YSmbhQxk-/exec";
 
